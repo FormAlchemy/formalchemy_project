329d5bb77abaefc702fd23f61d967c6c899e99db
Gael Pasgrimaud <gael@gawel.org>
2011-06-23 16:01:05 +0200

    add extjs

--- formalchemy_project/__init__.py
+++ formalchemy_project/__init__.py
@@ -14,8 +14,10 @@ def main(global_config, **settings):
     # pyramid_formalchemy's configuration
     config.include('pyramid_formalchemy')
     config.include('fa.jquery')
+    config.include('fa.extjs')
 
     # Admin UI (Used for the demo. Not really useful here...)
+    config.formalchemy_admin('/extjs', package='formalchemy_project', view='fa.extjs.ModelView')
     config.formalchemy_admin('/admin', package='formalchemy_project', view='fa.jquery.pyramid.ModelView')
 
     # Article admin UI. Use a custom query_factory to filter by user
